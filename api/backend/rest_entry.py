88ca93d70b64c16c90a42b8282fbf449f735837f
Logan Gatch <logangatch@Logans-MacBook-Pro.local>
2025-12-04 18:37:12 -0500

    api -analytics: added analytics blueprint with stub routes and todos

--- api/backend/rest_entry.py
+++ api/backend/rest_entry.py
@@ -5,6 +5,7 @@ import logging
 from logging.handlers import RotatingFileHandler
 from backend.admin_routes import admin_bp
 from backend.social_routes import social_bp
+from backend.analytics_routes import analytics_bp
 
 from backend.db_connection import db
 from backend.simple.simple_routes import simple_routes
@@ -54,6 +55,7 @@ def create_app():
     app.register_blueprint(ngos, url_prefix="/ngo")
     app.register_blueprint(admin_bp)
     app.register_blueprint(social_bp)
+    app.register_blueprint(analytics_bp)
 
     # Don't forget to return the app object
     return app
