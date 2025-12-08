4935ec43278b81a6e38b58de4d5f6361729be514
Edgar Castaneda <castaneda.e@northeastern.edu>
2025-12-08 18:24:03 -0500

    admin home page

--- app/src/pages/20_Admin_Home.py
+++ app/src/pages/20_Admin_Home.py
@@ -9,9 +9,22 @@ st.set_page_config(layout = 'wide')
 
 SideBarLinks()
 
-st.title('System Admin Home Page')
+st.title(f"Welcome to the Admin Dashboard, {st.session_state.get('first_name', 'William')}.")
+st.write('')
+st.write('')
+st.write('### What would you like to do today?')
 
-if st.button('Update ML Models', 
+if st.button('View Pending Approvals', 
              type='primary',
              use_container_width=True):
-  st.switch_page('pages/21_ML_Model_Mgmt.py')
+  st.switch_page('pages/21_Pending_Approvals.py')
+
+if st.button('View System Alerts', 
+             type='primary',
+             use_container_width=True):
+  st.switch_page('pages/22_System_Alerts.py')
+
+if st.button('View Flagged Activities', 
+             type='primary',
+             use_container_width=True):
+  st.switch_page('pages/23_Flagged_Activities.py')
